f18164175dd9b2321646f380338473e3578e83c4
yyyujinnn <1209yujin@naver.com>
2024-03-13 03:25:06 +0900

    setting : App.js에 MainPage.jsx 연동

--- one-n/src/App.js
+++ one-n/src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
-// import { Outlet } from 'react-router-dom';
+import MainPage from './pages/MainPage/MainPage';
 
 function App() {
   return (
     <div className="App">
-      2024 벚꽃톤 13팀 N분의1
-      리액트 실행 후 첫페이지
-
-      {/* <Outlet /> */}
+      <MainPage />
     </div>
   );
 }
