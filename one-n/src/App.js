6f8db0506b636204f39f514a62eedc558135b5be
yyyujinnn <1209yujin@naver.com>
2024-03-13 03:14:28 +0900

    setting : 기본 폴더/파일 세팅

--- one-n/src/App.js
+++ one-n/src/App.js
@@ -1,9 +1,13 @@
-import './App.css';
+import React from 'react';
+// import { Outlet } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       2024 벚꽃톤 13팀 N분의1
+      리액트 실행 후 첫페이지
+
+      {/* <Outlet /> */}
     </div>
   );
 }
---
+++ one-n/src/pages/Explore/Explore.jsx
@@ -0,0 +1,12 @@
+const Explore = () => {
+    return (
+      <div>  
+        <div>
+            사진 목록 무한스크롤
+            사진 클릭시 레시피 상세페이지로 이동
+        </div>
+      </div>
+    );
+  };
+  export default Explore;
+  
