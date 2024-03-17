07876f351edc3191a86afea5b661e98abf90b439
yyyujinnn <1209yujin@naver.com>
2024-03-18 01:03:36 +0900

    feat: 메인페이지 로고 삽입,

--- one-n/src/pages/MainPage/MainPage.jsx
+++ one-n/src/pages/MainPage/MainPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './MainPage.css'
+import logo from '../../assets/logo/logo.png'
 import search from '../../assets/icons/search.svg'
 import mypage from '../../assets/icons/mypage.svg'
 import next from '../../assets/icons/next.svg'
@@ -10,45 +11,48 @@ import level from '../../assets/icons/level.svg'
 function MainPage() {
   return (
     <div>
-      <div className='header'>
-        {/* 로고 */}
+      <div className='header-icon'>
+        <img src={logo} alt='logo' class='logo-img' />
+
         <div className='right-header' >
           <img src = {search} alt='search' />
           <img src = {mypage} alt='mypage' />
         </div>
+
       </div>
-
-<div className='main-body'>
-      <div className='title'>
-        <div className='recipe-text'>
-          <div className='recipe-name'> 레시피 둘러보기 </div>
-          <div className='more'> 더보기 <img src={next} alt='next'/> </div>
-        </div>
-
-        <div className='recipePhoto'>
-          <div className='photoStyle'> 
-            <img src={like} alt='like' />
-            <span> 카레 </span> 
+      
+      <div className='main-body'>
+        <div className='title'>
+          <div className='recipe-text'>
+            <div className='recipe-name'> 레시피 둘러보기 </div>
+            <div className='more'> 더보기 <img src={next} alt='next'/> </div>
           </div>
 
-          <div className='photoStyle'> 
-            <img src={like} alt='like' />
-            <span> 까르보나라 </span> 
-          </div>
+          <div className='recipePhoto'>
+            <div className='photoStyle'> 
+              <img src={like} alt='like' />
+              <span> 카레 </span> 
+            </div>
+
+            <div className='photoStyle'> 
+              <img src={like} alt='like' />
+              <span> 까르보나라 </span> 
+            </div>
           
-          <div className='photoStyle'> 
-            <img src={like} alt='like' />
-            <span> 불닭볶음면 </span> 
+            <div className='photoStyle'> 
+              <img src={like} alt='like' />
+              <span> 불닭볶음면 </span> 
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className='title'>
-        <div className='recipe-text'>
-          <div className='grd-name'> 지금 핫한 식품 </div>
-          <div className='more'> 더보기 <img src={next} alt='next'/> </div>
-        </div>
+        <div className='title'>
+          <div className='recipe-text'>
+            <div className='grd-name'> 지금 핫한 식품 </div>
+            <div className='more'> 더보기 <img src={next} alt='next'/> </div>
+          </div>
 
+        {/* 추후 컴포넌트로 변경 */}
         <div className='saleGrd'>
           <div className='grd-img' />
           
