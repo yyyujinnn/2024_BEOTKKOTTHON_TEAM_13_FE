7c8a8c7a65fdbb8ef3158a7c7850e51791436c73
yyyujinnn <1209yujin@naver.com>
2024-03-21 01:19:53 +0900

    feat: 탐색페이지 이미지 api 연동

--- one-n/src/pages/Explore/Explore.jsx
+++ one-n/src/pages/Explore/Explore.jsx
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './Explore.css'
 import search from '../../assets/icons/search.svg'
 import Masonry from "https://cdn.skypack.dev/react-masonry-css@1.0.16";
-import a from './a.jpeg';
-import b from './b.jpeg';
-import c from './c.jpeg';
-import d from './d.jpeg';
-import e from './e.jpeg';
-import f from './f.jpeg';
 
 const Explore = () => {
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+      // API 엔드포인트 URL 설정
+      const apiUrl = 'http://20.39.188.154:8080/recipe/list?keyword=&page=';
+  
+      axios.get(apiUrl)
+        .then((response) => {
+          const updatedData = response.data.map(item => ({
+            ...item,
+            thumbnail_image: `http://20.39.188.154${item.thumbnail_image}`
+          }));
+          setData(updatedData);
+        })
+        .catch((error) => {
+          console.error('API 요청 에러:', error);
+        });
+  }, []);
 
   return (
     <>
@@ -24,15 +38,12 @@ const Explore = () => {
         className="grid-container"
         columnClassName="column"
         >
-          <div className="grid-item"> <img src={a}/>  </div>
-          <div className="grid-item"> <img src={b}/> </div>
-          <div className="grid-item"> <img src={c}/></div>
-          <div className="grid-item"> <img src={d}/>  </div>
-          <div className="grid-item"> <img src={e}/> </div>
-          <div className="grid-item"> <img src={f}/></div>
-          <div className="grid-item"> <img src={a}/>  </div>
-          <div className="grid-item"> <img src={b}/> </div>
-          <div className="grid-item"> <img src={c}/></div>
+          {data.map((item) => (
+          <div key={item.id} className="grid-item">
+            <img src={item.thumbnail_image} alt={`Image ${item.title}`} />
+          </div>
+        ))}
+
       </Masonry>
     </>
   )
