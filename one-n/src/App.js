0a74b1ad2d85916de9623202b47a9055468f074d
yyyujinnn <1209yujin@naver.com>
2024-03-23 00:06:19 +0900

    fix: 하단바 제외 페이지

--- one-n/src/App.js
+++ one-n/src/App.js
@@ -20,6 +20,7 @@ import SearchPage from './pages/SearchPage/SearchPage';
 
 function App() {
   return (
+    
     <BrowserRouter>
       <MyContextProvider>
         <div className="App">
@@ -36,14 +37,20 @@ function App() {
             <Route path="/chat" element={<ChatList/>} />
             <Route path="/chatroom" element={<ChatRoom/>} />
             <Route path='/explore' element={<Explore/>} />
-            <Route path='/recipe' element={<RecipeDetail/>} />
+            <Route path='/recipe/:recipeId' element={<RecipeDetail/>} />
             <Route path='/recipeRegister' element={<RecipeReg/>} />
             <Route path='/search' element={<SearchPage/>}/>
           </Routes>
 
         </div>
       </MyContextProvider>
-      {window.location.pathname !== '/chatroom' && <NavBar />}
+      {window.location.pathname !== '/chatroom' &&
+       window.location.pathname !== '/map' &&
+       window.location.pathname !== '/product-detail' &&
+       window.location.pathname !== '/product-post' &&
+       window.location.pathname !== '/recipe/:recipeId' &&
+       window.location.pathname !== '/recipeRegister' 
+       && <NavBar />}
     </BrowserRouter >
 
   );
